e097ffb824ea6036a2594b05c6e96f16ef994bcc
Saifansari-ai <saifansari300b@gmail.com>
2025-09-19 15:45:46 +0530

    app structure done

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ st.set_page_config(page_title="AI Assistant")
 
 st.title("Job Search :red[_AI Assistant_] :blue[:robot:]")
 
-st.markdown("""
-    <style>
-    .stTextInput > label {
-        font-size: 120%; /* Adjust this value as needed */
-        font-weight: bold; /* Optional: make the label bold */
-    }
-    </style>
-""", unsafe_allow_html=True)
-
 job_role = st.text_input(label=r"$\textsf{\Large Enter The Job Role You Are Interested}$",placeholder="eg. AI Engineer")
 
 if job_role:
