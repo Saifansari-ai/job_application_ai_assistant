ee807214cbe00430d12535766e2c44e7c8a153e3
Saifansari-ai <saifansari300b@gmail.com>
2025-09-20 09:40:59 +0530

    app structure updated

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ import streamlit as st
 
 st.set_page_config(page_title="AI Assistant")
 
-st.title("Job Search :red[_AI Assistant_] :blue[:robot:]")
+st.header("Job Search :red[_AI Assistant_] :blue[:robot:]")
 
-job_role = st.text_input(label=r"$\textsf{\Large Enter The Job Role You Are Interested}$",placeholder="eg. AI Engineer")
+if 'gemini_api_key' not in st.session_state:
+    st.session_state.gemini_api_key = ""
 
-if job_role:
-    st.write(f" You job role is {job_role} and now i am searching job roles related to {job_role}")
+def set_sidebar():
+        with st.sidebar:
+            st.subheader("API Configuration")
+            gemini_api_key = st.text_input("Enter you Gemini API key:",type="password")
+            
+            if st.button("Done"):
+                if gemini_api_key:
+                    st.session_state.gemini_api_key = gemini_api_key
+                    st.success("API Key Saved!")
+                else:
+                    st.warning("Please fill the gemini api key first")
+
+def main():
+    set_sidebar()
+    if not st.session_state.gemini_api_key:
+        st.warning("Please configure your API keys in the sidebar first")
+        return
+    job_role = st.text_input(label=r"$\textsf{\Large Enter The Job Role You Are Interested}$",placeholder="eg. AI Engineer")
+
+    if job_role:
+        st.write(f" You job role is {job_role} and now i am searching job roles related to {job_role}")
         
+if __name__ == "__main__":
+    main()
